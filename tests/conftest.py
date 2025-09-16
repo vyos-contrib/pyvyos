a31d1f21d33ce348f89bc5b0bc05835529425480
eduardormorais <eduardoromorais@gmail.com>
2025-09-16 13:55:02 -0300

    Fix/979201165- Including default valu for environment variables in unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,11 +8,11 @@ from pyvyos import VyDevice
 @pytest.fixture(scope="module")
 def config_device():
     return {
-        "hostname": os.getenv("VYDEVICE_HOSTNAME"),
-        "apikey": os.getenv("VYDEVICE_APIKEY"),
-        "port": os.getenv("VYDEVICE_PORT"),
-        "protocol": os.getenv("VYDEVICE_PROTOCOL"),
-        "verify": os.getenv("VYDEVICE_VERIFY_SSL"),
+        "hostname": os.getenv("VYDEVICE_HOSTNAME", "localhost"),
+        "apikey": os.getenv("VYDEVICE_APIKEY", "api_key"),
+        "port": os.getenv("VYDEVICE_PORT", 443),
+        "protocol": os.getenv("VYDEVICE_PROTOCOL", "https"),
+        "verify": os.getenv("VYDEVICE_VERIFY_SSL", False),
         "timeout": 0,
     }
 
